9f97bb150ff2db397c7919f0c985c416f7778179
Godfrey Jonas <jonasgodfrey@users.noreply.github.com>
2021-05-11 06:38:55 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,7 @@ use App\Models\Cbo;
 use App\Models\Ward;
 use App\Models\HealthFacility;
 use App\Models\Remedial;
+// use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -38,6 +39,8 @@ class HomeController extends Controller
 
         if ($role == "Admin") {
 
+            // $password = HASH::make('Adamawa');
+            // dd($password);
             $states =  count(States::where('status', 'active')->get());
             $lgas =  count(Lgas::where('status', 'active')->get());
             $wards = count(Ward::where('status', 'active')->get());
